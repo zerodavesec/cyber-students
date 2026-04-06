592af41edd13002ffb0da1100c04fcff5843fb6a
David Martin <zerodavesecurity@proton.me>
2026-04-06 11:19:37 +0100

    Testing CI

--- run_test.py
+++ run_test.py
@@ -1,12 +1,14 @@
 import logging
 import unittest
 
+## Adding a comment here so I can have a change in files and then commit to a branch
+## -> It should create a PR and tests should pass.
 from test.login import LoginHandlerTest
 from test.logout import LogoutHandlerTest
 from test.registration import RegistrationHandlerTest
 from test.user import UserHandlerTest
 from test.welcome import WelcomeHandlerTest
 
-if __name__ == '__main__':
-    logging.getLogger('tornado.access').disabled = True
+if __name__ == "__main__":
+    logging.getLogger("tornado.access").disabled = True
     unittest.main()
