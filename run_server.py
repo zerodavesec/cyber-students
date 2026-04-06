0e350889ee310adbc2b48348b91eb05597361e2a
David Martin <zerodavesecurity@proton.me>
2026-04-06 12:00:54 +0100

    Created Pepper initialisation via os.urandom() using 32 bytes for the size

--- run_server.py
+++ run_server.py
@@ -1,18 +1,31 @@
-from logging import basicConfig, INFO, info
+import os
+from logging import INFO, basicConfig, info
+
+import keyring
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
-from api.conf import PORT
 from api.app import Application
+from api.conf import PORT
+
+
+def initialise_pepper() -> None:
+    if keyring.get_password("cyberstudents", "pepper") is None:
+        pepper: str = os.urandom(32).hex()
+        keyring.set_password("cyberstudents", "pepper", pepper)
+
 
 def main():
     basicConfig(level=INFO)
 
+    initialise_pepper()
+
     http_server = HTTPServer(Application())
     http_server.listen(PORT)
 
-    info('Starting server on port ' + str(PORT) + '...')
+    info("Starting server on port " + str(PORT) + "...")
     IOLoop.current().start()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
