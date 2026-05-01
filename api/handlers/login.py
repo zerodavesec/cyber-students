25c9de180e0ce7e0631bc39724b9b9e40e90b4bb
David Martin <zerodavesecurity@proton.me>
2026-05-01 14:45:13 +0100

    Adding Logic to hash the token before sending to DB, but returning plaintext for printing on CLI

--- api/handlers/login.py
+++ api/handlers/login.py
@@ -7,21 +7,40 @@ from cryptographic_operations.passphrase_operations import (
     SCRYPT_DERIVATION_PARAMS,
     passphrase_verification,
 )
+from cryptographic_operations.token_operations import sha256_string_hashing
 
 from .base import BaseHandler
 
 
 class LoginHandler(BaseHandler):
-    async def generate_token(self, email):
-        token_uuid = uuid4().hex
-        expires_in = (datetime.now(timezone.utc) + timedelta(hours=2)).timestamp()
+    async def generate_token(self, email) -> dict[str, str | float]:
+        # OWASP requires at least 64bits of entropy to prevent brute-force session guessing attacks
+        # https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html
+        # UUIDv4 meets this as it is created randomly from the uuid4() declaration:,
+        # def uuid4():
+        #     """Generate a random UUID."""
+        #     int_uuid_4 = int.from_bytes(os.urandom(16))
+        #     int_uuid_4 &= _RFC_4122_CLEARFLAGS_MASK
+        #     int_uuid_4 |= _RFC_4122_VERSION_4_FLAGS
+        #     return UUID._from_int(int_uuid_4)
+        #
+        # The final result is that uuid4 created with Python has 122 bits of entropy and
+        # 6 hardcoded bits (deterministic). An attacker would need to brute force a 2**122 space
 
-        token = {
-            "token": token_uuid,
+        token_uuid: str = uuid4().hex
+        hashed_uuid: str = sha256_string_hashing(token_uuid)
+        expires_in: float = (
+            datetime.now(timezone.utc) + timedelta(hours=2)
+        ).timestamp()
+
+        token: dict[str, str | float] = {"token": token_uuid, "expiresIn": expires_in}
+
+        hashed_token: dict[str, str | float] = {
+            "token": hashed_uuid,
             "expiresIn": expires_in,
         }
 
-        await self.db.users.update_one({"email": email}, {"$set": token})
+        await self.db.users.update_one({"email": email}, {"$set": hashed_token})
 
         return token
 
