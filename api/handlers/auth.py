bb32366870c212d072ba7f79c7287581cee21bcd
David Martin <zerodavesecurity@proton.me>
2026-05-01 14:59:46 +0100

    Adding logic to hash tokens before checking whether they exist in the DB

--- api/handlers/auth.py
+++ api/handlers/auth.py
@@ -1,44 +1,44 @@
 from datetime import datetime, timezone
 
+from cryptographic_operations.token_operations import sha256_string_hashing
+
 from .base import BaseHandler
 
-class AuthHandler(BaseHandler):
 
+class AuthHandler(BaseHandler):
     async def prepare(self):
         super(AuthHandler, self).prepare()
 
-        if self.request.method == 'OPTIONS':
+        if self.request.method == "OPTIONS":
             return
 
         try:
-            token = self.request.headers.get('X-Token')
+            token = self.request.headers.get("X-Token")
             if not token:
-              raise Exception()
+                raise Exception()
         except Exception:
             self.current_user = None
-            self.send_error(400, message='You must provide a token!')
+            self.send_error(400, message="You must provide a token!")
             return
 
-        user = await self.db.users.find_one({
-            'token': token
-        }, {
-            'email': 1,
-            'displayName': 1,
-            'expiresIn': 1
-        })
+        hashed_token: str = sha256_string_hashing(token)
+        print(hashed_token)
+        user = await self.db.users.find_one(
+            {"token": hashed_token}, {"email": 1, "displayName": 1, "expiresIn": 1}
+        )
 
         if user is None:
             self.current_user = None
-            self.send_error(403, message='Your token is invalid!')
+            self.send_error(403, message="Your token is invalid!")
             return
 
         current_time = datetime.now(timezone.utc).timestamp()
-        if current_time > user['expiresIn']:
+        if current_time > user["expiresIn"]:
             self.current_user = None
-            self.send_error(403, message='Your token has expired!')
+            self.send_error(403, message="Your token has expired!")
             return
 
         self.current_user = {
-            'email': user['email'],
-            'display_name': user['displayName']
+            "email": user["email"],
+            "display_name": user["displayName"],
         }
