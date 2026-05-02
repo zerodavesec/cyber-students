7635c07c3ced0518104e1f1db08395e3910e5e3d
David Martin <zerodavesecurity@proton.me>
2026-05-02 10:33:07 +0100

    Fixing tests after implementing token hashing.

--- test/logout.py
+++ test/logout.py
@@ -1,70 +1,84 @@
 from json import dumps
+
 from tornado.escape import json_decode
 from tornado.httputil import HTTPHeaders
 from tornado.ioloop import IOLoop
 from tornado.web import Application
 
 from api.handlers.logout import LogoutHandler
+from cryptographic_operations.token_operations import sha256_string_hashing
 
 from .base import BaseTest
 
-class LogoutHandlerTest(BaseTest):
 
+class LogoutHandlerTest(BaseTest):
     @classmethod
     def setUpClass(self):
-        self.my_app = Application([(r'/logout', LogoutHandler)])
+        self.my_app = Application([(r"/logout", LogoutHandler)])
         super().setUpClass()
 
     async def register(self):
-        await self.get_app().db.users.insert_one({
-            'email': self.email,
-            'password': self.password,
-            'displayName': 'testDisplayName'
-        })
+        await self.get_app().db.users.insert_one(
+            {
+                "email": self.email,
+                "password": self.password,
+                "displayName": "testDisplayName",
+            }
+        )
 
     async def login(self):
-        await self.get_app().db.users.update_one({
-            'email': self.email
-        }, {
-            '$set': { 'token': self.token, 'expiresIn': 2147483647 }
-        })
+        await self.get_app().db.users.update_one(
+            {"email": self.email},
+            {
+                "$set": {
+                    "token": sha256_string_hashing(self.token),
+                    "expiresIn": 2147483647,
+                }
+            },
+        )
 
     def setUp(self):
         super().setUp()
 
-        self.email = 'test@test.com'
-        self.password = 'testPassword'
-        self.token = 'testToken'
+        self.email = "test@test.com"
+        self.password = "testPassword"
+        self.token = "testToken"
 
         IOLoop.current().run_sync(self.register)
         IOLoop.current().run_sync(self.login)
 
     def test_logout(self):
-        headers = HTTPHeaders({'X-Token': self.token})
+        headers = HTTPHeaders({"X-Token": self.token})
         body = {}
 
-        response = self.fetch('/logout', headers=headers, method='POST', body=dumps(body))
+        response = self.fetch(
+            "/logout", headers=headers, method="POST", body=dumps(body)
+        )
         self.assertEqual(200, response.code)
 
     def test_logout_without_token(self):
         body = {}
 
-        response = self.fetch('/logout', method='POST', body=dumps(body))
+        response = self.fetch("/logout", method="POST", body=dumps(body))
         self.assertEqual(400, response.code)
 
     def test_logout_wrong_token(self):
-        headers = HTTPHeaders({'X-Token': 'wrongToken'})
+        headers = HTTPHeaders({"X-Token": "wrongToken"})
         body = {}
 
-        response = self.fetch('/logout', method='POST', body=dumps(body))
+        response = self.fetch("/logout", method="POST", body=dumps(body))
         self.assertEqual(400, response.code)
 
     def test_logout_twice(self):
-        headers = HTTPHeaders({'X-Token': self.token})
+        headers = HTTPHeaders({"X-Token": self.token})
         body = {}
 
-        response = self.fetch('/logout', headers=headers, method='POST', body=dumps(body))
+        response = self.fetch(
+            "/logout", headers=headers, method="POST", body=dumps(body)
+        )
         self.assertEqual(200, response.code)
 
-        response_2 = self.fetch('/logout', headers=headers, method='POST', body=dumps(body))
+        response_2 = self.fetch(
+            "/logout", headers=headers, method="POST", body=dumps(body)
+        )
         self.assertEqual(403, response_2.code)
